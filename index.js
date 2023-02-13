008c226fca0d3775504feb48f9ea4a133f8efc0a
vaishnavihole <vaishnavihole1@gmail.com>
2023-02-14 00:14:25 +0530

    use variables

---
+++ index.js
@@ -0,0 +1,16 @@
+// 1. Ways to print in JavaScript
+//  console.log("Hello Word")
+// alert("me")
+// document.write("this is document write")
+
+// 2.javascript console API 
+// console.log("Hello Word", 4 + 6, "Another log");
+// console.warn("this is warning");
+// console.warn("this is an error");
+
+// 3. Javascript Variables
+// what are Variables? - Containers to store data values
+
+var number1 = 25;
+var number2 = 34;
+console.log(number1 + number2);
