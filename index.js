d37b3a471c7b7d272af36d6a02478a7b27936d9f
vaishnavihole <vaishnavihole1@gmail.com>
2023-02-14 15:40:28 +0530

    use function

--- index.js
+++ index.js
@@ -90,6 +90,13 @@ let y = 56;
 // console.log(false || true)
 
 // logical not
- console.log(!false);
- console.log(!true);
- 
+//  console.log(!false);
+//  console.log(!true);
+
+
+ function avg(a, b){
+    return (a + b)/2;
+ }
+c1 = avg(4, 6);
+c2 = avg(14, 16);
+console.log(c1, c2)
