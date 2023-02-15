f543737e1d23fdd229d6b3954b442c478624a8e3
vaishnavihole <vaishnavihole1@gmail.com>
2023-02-15 09:11:27 +0530

    use continue statement

--- index.js
+++ index.js
@@ -180,6 +180,15 @@ let arr1 = [1, 2, 3, 4, 5, 6, 7];
      console.log(arr[i]);
  }
 
+ // Continue statement
+ let arr2 = [1, 2, 3, 4, 5, 6, 7];
+ for(let i = 0;i<arr1.length;i++){
+    if(i==2){
+        continue;
+    }
+     console.log(arr[i]);
+ }
+
 
 
 
