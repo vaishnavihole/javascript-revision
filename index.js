4345da517477857e52bf2330e6e2c9c2621637cd
vaishnavihole <vaishnavihole1@gmail.com>
2023-02-16 08:57:01 +0530

    use DOM manipulation getElementByClassName

--- index.js
+++ index.js
@@ -249,6 +249,11 @@ let elem = document.getElementById('click');
 console.log(elem);
 
 
+let elemClass = document.getElementsByClassName('container');
+console.log(elemClass);
+elemClass[0].style.background = "yellow";
+
+
 
 
 
